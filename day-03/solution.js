d0d4925fe0c56116a5503529e55d7bfcd1151d24
Vinay Nath <vinayfour@gmail.com>
2025-12-07 10:03:05 +1100

    Day 3 - added soln 3

--- day-03/solution.js
+++ day-03/solution.js
@@ -1,11 +1,11 @@
+/*
 // Soln 1 using Set
-
 const arr = [1, 2, 2, 3, 3, 3, 4];
 let uniqueArr = [...new Set(arr)]; // ie uniqueArr = new Set(arr); then convert obj back to arr -> const newVar = [...uniqueArr] -> log newVar.
 console.log(uniqueArr);
 
-// Soln 2 using filter
 
+// Soln 2 using filter
 const letters = ['a', 'b', 'c', 'c', 'c', 'd', 'a', 'b'];
 
 function removeDuplicates(arr) {
@@ -13,3 +13,20 @@ function removeDuplicates(arr) {
 }
 
 console.log(removeDuplicates(letters));
+*/
+
+// Soln 3 using loop without filter
+const numbers = [1, 1, 2, 2, 3, 3];
+
+function uniqueElements(arr) {
+  let result = [];
+
+  for (let item of arr) {
+    if (!result.includes(item)) {
+      result.push(item);
+    }
+  }
+  return result;
+}
+
+console.log(uniqueElements(numbers));
