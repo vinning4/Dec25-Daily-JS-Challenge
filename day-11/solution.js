40ffead430233734bab4d0aa27db5e7d9bfa7425
Vinay Nath <vinayfour@gmail.com>
2025-12-15 16:51:02 +1100

    Updating readme

---
+++ day-11/solution.js
@@ -0,0 +1,12 @@
+function combine(arr1, arr2) {
+  if (arr1.length === 0) { return arr1; }
+  if (arr2.length === 0) { return arr2; }
+  
+  let merged = [...arr1, ...arr2];
+  for (let item of merged) {
+    if 
+  }
+
+}
+
+console.log(combine([1,2,3], [3,4,5]));
