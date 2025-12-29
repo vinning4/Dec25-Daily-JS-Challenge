b0cb1b2ff1d51f962daabd7dcb4b17011733b140
Vinay Nath <vinayfour@gmail.com>
2025-12-29 13:10:58 +1100

    Day 25 soln 2

--- day-25/solution.js
+++ day-25/solution.js
@@ -20,3 +20,17 @@ console.log(calc.subtract(1,3));
 console.log(calc.multiply(1,3));
 console.log(calc.divide(1,0));
 console.log(calc.divide(1,2));
+
+// Soln 2 using factory func
+function createCalc() {
+  return {
+    add: (a,b) => a + b,
+    subtract: (a,b) => a - b,
+    multiply: (a,b) => a * b,
+    divide: (a,b) => b == 0 ? "Divide by zero" : a/b
+  }
+}
+
+const result = new createCalc();
+console.log(result.divide(5,5));   // Can use result or full func call createCalc() like next line.
+console.log(createCalc().multiply(5,5));
